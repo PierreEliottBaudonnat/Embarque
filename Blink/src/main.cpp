c7d5991d42787b592c560f18b1300bcc1f59e77a
Pierre-Eliott <p.baudonnat@epsi.fr>
2022-01-05 12:44:27 +0100

    Change some configurations

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -10,9 +10,17 @@ void setup() {
   pinMode(led, OUTPUT);    
 }
 
+void ledOn() {
+  PORTB = PORTB | (1 << 5);
+}
+
+void ledOff() {
+  PORTB = PORTB & ~ (1 << 5);
+}
+
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  ledOn();
+  delay(1000);
+  ledOff();
+  delay(1000);
 }
