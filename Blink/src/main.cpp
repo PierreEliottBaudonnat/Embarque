de5c9b22b2232eb5baafe622707e2231a6d52630
Pierre-Eliott <p.baudonnat@epsi.fr>
2022-01-13 15:57:46 +0100

    adding timerSetup function and ISR

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -9,9 +9,6 @@ void ledSetup() {
 }
 
 // the setup routine runs once when you press reset:
-void setup() {                
-  ledSetup();    
-}
 
 void ledOn() {
   PORTB = PORTB | (1 << 5);
@@ -25,7 +22,22 @@ void toggle(){
   PINB |= (1<<5);
 }
 
-void loop() {
+void timerSetup(){
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
+  OCR1A = 15625;
+  TCNT1 = 0;
+  TIMSK1 = (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect){
   toggle();
-  delay(1000);
+}
+
+void setup() {                
+  ledSetup(); 
+  timerSetup();   
+}
+
+void loop() {
 }
