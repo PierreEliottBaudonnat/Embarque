208a4911d648896fad3c5c1e3a06dc296b1cac06
Pierre-Eliott <p.baudonnat@epsi.fr>
2022-01-27 10:53:10 +0100

    Modifying and mesuring the distance between an obstacle

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -11,6 +11,7 @@ void setup(){
   pinMode (trigger_pin, OUTPUT); 
   pinMode (echo_pin, INPUT);
   pinMode (buzzer_pin, OUTPUT);
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
 }
 
@@ -20,13 +21,17 @@ void loop(){
   delayMicroseconds (10);
   digitalWrite (trigger_pin, LOW);
   time = pulseIn (echo_pin, HIGH);
-  distance = (time * 0.034);
+  distance = (time * 0.034)/2;
 
-  if (distance <= 20) {       
+  if (distance <= 10) {    
+    Serial.print (" Distance = ");              
+    Serial.println (distance);   
     digitalWrite(led, HIGH);
     delay (500);
   }
-  else {     
+  else {   
+    Serial.print (" Distance = ");              
+    Serial.println (distance);  
     digitalWrite(led, LOW);
     delay (500);        
   } 
