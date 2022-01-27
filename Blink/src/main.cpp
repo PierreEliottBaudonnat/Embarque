5de7df13b88205058c12c30c1ccc69f600628fc8
Pierre-Eliott <p.baudonnat@epsi.fr>
2022-01-27 12:58:53 +0100

    add some constants and comment to explain the code

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -1,38 +1,50 @@
 #include <Arduino.h>
-
-int trigger_pin = 2;
-int echo_pin = 3;
-int buzzer_pin = 10; 
-int led =  13;
-int time;
-int distance; 
-
-void setup(){
-  pinMode (trigger_pin, OUTPUT); 
-  pinMode (echo_pin, INPUT);
-  pinMode (buzzer_pin, OUTPUT);
+ 
+const byte TRIGGER_PIN = 2; // Broche TRIGGER
+const byte ECHO_PIN = 3;    // Broche ECHO
+const byte LED_PIN = 12;  // Broche LED
+ 
+ 
+/* Vitesse du son dans l'air en mm/microsecondes */
+const float SOUND_SPEED = 340.0 / 1000;
+ 
+void setup() {
+  /* Initialise le port serie */
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  /* Initialise les broches */
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit etre LOW au repos
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  pinMode(ECHO_PIN, INPUT);
 }
-
-
-void loop(){
-  digitalWrite (trigger_pin, HIGH);
-  delayMicroseconds (10);
-  digitalWrite (trigger_pin, LOW);
-  time = pulseIn (echo_pin, HIGH);
-  distance = (time * 0.034)/2;
-
-  if (distance <= 10) {    
-    Serial.print (" Distance = ");              
-    Serial.println (distance);   
-    digitalWrite(led, HIGH);
-    delay (500);
+ 
+void loop() {
+ 
+  /* Mesure de distance avec une impulsion HIGH sur la broche TRIGGER */
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+ 
+  /* Mesure du temps entre l'envoi de l'impulsion et sa réception */
+  long measure = pulseIn(ECHO_PIN, HIGH);
+ 
+  /* Calcule la distance grâce au temps mesuré */
+  float distance_mm = measure / 2.0 * SOUND_SPEED;
+ 
+  if(distance_mm/1000 < 0.10){ 
+    digitalWrite(LED_PIN, HIGH); // si un obstacle est à moins de 10cm, allume la LED
   }
-  else {   
-    Serial.print (" Distance = ");              
-    Serial.println (distance);  
-    digitalWrite(led, LOW);
-    delay (500);        
-  } 
+  else{
+    digitalWrite(LED_PIN, LOW);
+  }
+ 
+  // Affiche des résultats en m 
+  Serial.print("Distance: ");
+  Serial.println(distance_mm / 1000.0, 2);
+  Serial.print("m");
+ 
+ 
+  /* Délai d'attente pour ne pas trop afficher de résultats */
+  delay(500);
 }
