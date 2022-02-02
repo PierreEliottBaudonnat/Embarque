6a3bbb0d770b56fcdfe95366903a577e90e2ee1f
Pierre-Eliott <p.baudonnat@epsi.fr>
2022-02-02 15:49:02 +0100

    Ajout du code pour les changements de directions si un obstacle est rencontré. Le projet est fonctionnel à ce stade

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -14,6 +14,8 @@ int MA1 = 11;
 // Bits pour le moteur B
 int MB0 = 13;
 int MB1 = 12;
+
+long randomNumber;  // Nombre random pour choisir de partir à droite ou à gauche
  
 /* Vitesse du son dans l'air en mm/microsecondes */
 const float SOUND_SPEED = 340.0 / 1000;
@@ -44,8 +46,74 @@ void goForward(){
   // On indique au moteur B de mettre la marche avant
   digitalWrite(MB0, LOW);
   digitalWrite(MB1, HIGH);
+
+  // Il n'y a pas de delay, la roue tourne infiniement tant qu'elle ne rencontre pas d'obstacle
+}
+
+void stop(){
+  // On indique au moteur A de se stopper
+  digitalWrite(MA0, LOW);
+  digitalWrite(MA1, LOW);
+  
+  // On indique au moteur B de se stopper
+  digitalWrite(MB0, LOW);
+  digitalWrite(MB1, LOW);
+
+  // On se stoppe pendant une demi-seconde
+  delay(500);
+}
+
+void goBackward(){
+  // On indique au moteur A de faire marche arrière
+  digitalWrite(MA0, HIGH);
+  digitalWrite(MA1, LOW);
+  
+  // On indique au moteur B de faire marche arrière
+  digitalWrite(MB0, HIGH);
+  digitalWrite(MB1, LOW); 
+
+  // On fait marche arrière pendant une seconde et demi
+  delay(1500);
+}
+
+void turnOnTheLeft(){
+  // Le moteur A fait la marche avant
+  digitalWrite(MA0, LOW);
+  digitalWrite(MA1, HIGH);
+  
+  // Le moteur B fait la marche arrière
+  digitalWrite(MB0, HIGH);
+  digitalWrite(MB1, LOW);
+  
+  delay(750);
+}
+
+void turnOnTheRight(){
+  // Le moteur A fait la marche arrière
+  digitalWrite(MA0, HIGH);
+  digitalWrite(MA1, LOW);
+  
+  // Le moteur B fait la marche avant
+  digitalWrite(MB0, LOW);
+  digitalWrite(MB1, HIGH);
+  
+  delay(750);
 }
  
+void changeDirection(){
+  stop();
+  goBackward();
+  randomNumber = random(2.0);
+  Serial.print(randomNumber);
+  if(randomNumber < 1.0){
+    turnOnTheLeft();
+  }
+  else{
+    turnOnTheRight();
+  }
+  stop();
+}
+
 void loop() {
  
   /* Mesure de distance avec une impulsion HIGH sur la broche TRIGGER */
@@ -61,13 +129,12 @@ void loop() {
  
   if(distance/1000 < 0.10){
     Serial.print("Vous êtes à moins de 10cm d'un obstacle, il faut s'arrêter"); // si un obstacle est à moins de 10cm, on change de direction
-    //changeDirection();
+    changeDirection();
   }
   else{
     Serial.print("Vous pouvez continuer en ligne droite !"); // sinon, on va tout droit
     goForward();
   }
- 
-  /* Délai d'attente pour ne pas trop afficher de résultats */
+
   delay(500);
 }
